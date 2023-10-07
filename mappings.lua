91c765e0127dbc21d18bac3a2d9ff12f5b4eff20
parzevaI <66918776+parzevaI@users.noreply.github.com>
2023-10-07 00:12:08 -0400

    Rust bar cargo check, checks for cargo.toml

--- mappings.lua
+++ mappings.lua
@@ -39,15 +39,24 @@ local commands = {
   python = function(filepath) return "python3 "..filepath end,
   javascript = function(filepath) return "node "..filepath end,
   html = function(filepath) return "open "..filepath end,
+
   rust = function(filepath)
-    local filename = string.match(filepath, "[^/]+$"):sub(1,-4)
-    local filedir = filepath:sub(1, filepath:match(".+()/"))
-    print(filedir)
-    if string.find(filedir, "/src/") then
-      return "(cd "..filedir.." && cargo run)"
+    local filename = string.match(filepath, "[^/]+$"):sub(1,-4) -- main
+    local filedir = filepath:sub(1, filepath:match(".+()/")) -- /Users/agoni/Documents/CompSci/test/check/src/
+    local dir_to_src = filedir:match("(.-)/src")
+    if dir_to_src then
+      local handle = io.popen("(cd "..dir_to_src.." && ls)") -- /Users/agoni/Documents/CompSci/test/check
+      local is_cargo = string.find(handle:read("*a"), "Cargo.toml")
+      handle:close()
+
+      if is_cargo then
+        return "(cd "..filedir.." && cargo run)"
+      end
     end
+
     return "(cd "..filedir.." && rustc "..filepath.." && ./"..filename..")"
   end,
+
   lua = function(filepath) return "lua "..filepath end,
   java = function(filepath)
     local filename = string.match(filepath, "[^/]+$"):sub(1,-6)
@@ -56,7 +65,7 @@ local commands = {
   end,
   svelte = function(filepath)
     local filedir = filepath:sub(1, filepath:match(".+()/"))
-    return "(cd "..filedir.." && npmrd)"
+    return "(cd "..filedir.." && npm run dev)"
   end,
 }
 

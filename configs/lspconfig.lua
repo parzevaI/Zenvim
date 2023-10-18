b50813acbad1cbd8e765993c5ad519c11bfb293a
parzevaI <66918776+parzevaI@users.noreply.github.com>
2023-10-18 11:37:58 -0400

    Fixed fatal bug with tailwind LSP causing it to inialiate CPU on html files that don't have tailwind initialized

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -67,7 +67,17 @@ lspconfig.rust_analyzer.setup {
 lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"svelte", "html"}
+  filetypes = {"svelte", "html"},
+  hovers = true,
+  suggestions = true,
+  root_dir = function(fname)
+    local root_pattern = require("lspconfig").util.root_pattern(
+      "tailwind.config.cjs",
+      "tailwind.config.js",
+      "postcss.config.js"
+    )
+    return root_pattern(fname)
+  end,
 }
 
 

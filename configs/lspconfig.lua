d7abcc2a4f2413428f23e4098e3e9e9cc5dd4452
parzevaI <66918776+parzevaI@users.noreply.github.com>
2023-10-20 21:31:04 -0400

    fixed rust lsp issue where it didn't work on non cargo projects

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -54,7 +54,7 @@ lspconfig.rust_analyzer.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {"rust"},
-  root_dir = util.root_pattern("Cargo.toml"),
+  -- root_dir = util.root_pattern("Cargo.toml"),
   settings = {
     ['rust-analyzer'] = {
       cargo = {
